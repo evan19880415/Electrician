322b89db71abd5947d90c3d541a515c9986d2835
evan.chen <Evan.Chen@acer.com>
2014-02-12 16:58:01 +0800

    handle exception(PRODUCTION)

--- app/start/global.php
+++ app/start/global.php
@@ -52,13 +52,13 @@ App::error(function(Exception $exception, $code)
 	switch ($code)
     {
         case 403:
-            return Response::view('errors.403', array(), 403);
+            return Response::view('errors.default', array(), 403);
 
         case 404:
             return Response::view('errors.404', array(), 404);
 
         case 500:
-            return Response::view('errors.500', array(), 500);
+            return Response::view('errors.default', array(), 500);
 
         default:
             return Response::view('errors.default', array(), $code);
